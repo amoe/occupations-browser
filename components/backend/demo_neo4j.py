aad6979c645dcc83cee04bf53e03b2016662a692
David Banks <amoebae@gmail.com>
2018-05-08 17:15:56 +0100

    add the data set for the test

--- components/backend/demo_neo4j.py
+++ components/backend/demo_neo4j.py
@@ -12,8 +12,43 @@ READ_ENTIRE_GRAPH_QUERY = """
     MATCH (n) OPTIONAL MATCH (n)-[r]->() RETURN n, r
 """
 
+node_index = {}
+
+
+roots = [
+]
+
+roots_index = {
+    
+}
+
+
 with driver.session() as session:
     with session.begin_transaction() as tx:
         results = tx.run(READ_ENTIRE_GRAPH_QUERY)
         for record in results:
-            print(record)
+            node['content'] = record['n']
+
+
+# assert that 'ironmonger' is a root
+# assert that 'hold' is a root
+# assert that 'keep' is a root
+
+# given the data set provided by the shuffled set, which is as such
+
+["keep" "the" "Bell" "at" "Uxbridge"]
+["keep" "the" "Half-moon"]
+["hold" "a" "situation" "under" "the" "Honourable" "East" "India" "Company"]
+["ironmonger" "and" "founder"]
+
+
+
+
+
+# MATCH a WHERE NOT (a)-[:LOVES]->()
+
+
+# MATCH (n) WHERE NOT (n)<-[:PRECEDES]-() RETURN n;            
+
+
+
