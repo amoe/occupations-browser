de443dda8112bc6c02604dccf3fcf688c6ac3824
David Banks <amoebae@gmail.com>
2018-05-09 09:18:13 +0100

    test framework + abstraction for graph ops

--- components/backend/graph_operations.py
+++ components/backend/graph_operations.py
@@ -1,2 +1,23 @@
+import neo4j
+import neo4j.v1
+
+credentials = ('neo4j', 'password')
+uri = "bolt://localhost:7687"
+
+driver = neo4j.v1.GraphDatabase.driver(uri, auth=credentials)
+
+READ_ENTIRE_GRAPH_QUERY = """
+    MATCH (n) OPTIONAL MATCH (n)-[r]->() RETURN n, r
+"""
+
 def get_roots():
-    return []
+    roots = [
+        {
+            'content': 'foo'
+        }
+    ]
+
+    with driver.session() as session:
+        with session.begin_transaction() as tx:
+            results = tx.run(READ_ENTIRE_GRAPH_QUERY)
+            return roots
