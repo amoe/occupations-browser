d1b0ca1fec20ef5d4eabff332c31d8200c239da3
David Banks <amoebae@gmail.com>
2018-05-14 11:05:11 +0100

    new import script

--- scripts/import_data.py
+++ scripts/import_data.py
@@ -17,11 +17,33 @@ def run_some_query(query, parameters):
             results = tx.run(query, parameters)
             return results
 
+CREATE_PARENT_SENTENCE = """
+    CREATE
+      (s1:Sentence {tokens: {token_list}})
+"""
+
+MERGE_TOKEN_NODE = """
+    MERGE (n:Token {content: {content}})
+    RETURN n
+"""
+
+CREATE_CONTAINS_RELATIONSHIP = """
+    MATCH (n:Token {content: {content}}), (s1:Sentence {tokens: {token_list}})   
+    CREATE (s1)-[:CONTAINS {index: {index}}]->(n)
+"""
+
+
 for path in sys.argv[1:]:
     with open(path, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             tokens = nltk.word_tokenize(row['occupation_status'])
-            print(tokens)
+            result = run_some_query(CREATE_PARENT_SENTENCE, {'token_list': tokens})
 
-
+            for index, token in enumerate(tokens):
+                run_some_query(MERGE_TOKEN_NODE, {'content': token})
+                run_some_query(
+                    CREATE_CONTAINS_RELATIONSHIP,
+                    {'content': token, 'index': index, 'token_list': tokens}
+                )
+                
