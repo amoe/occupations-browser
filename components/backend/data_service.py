942ca940becf8e7b38c12899c5eaba70b188b2d7
David Banks <amoebae@gmail.com>
2018-07-23 13:13:54 +0100

    data service for creating sentences

--- components/backend/data_service.py
+++ components/backend/data_service.py
@@ -2,8 +2,13 @@ CREATE_PARENT_SENTENCE_STATEMENT = """
     CREATE (s1:Sentence {tokens: {token_seq}})
 """
 
-CREATE_TOKEN_STATEMENT = """
-    CREATE (t1:Token {node_properties})
+MERGE_TOKEN_STATEMENT = """
+   MERGE (t1:Token {content: {token_content}})
+"""
+
+CREATE_CONTAINS_RELATIONSHIP = """
+    MATCH (s1:Sentence {tokens: {token_seq}}), (t1:Token {content: {token_content}})
+    CREATE (s1)-[:CONTAINS {relationship_properties}]->(t1);
 """
 
 import misc
@@ -19,18 +24,27 @@ class DataService(object):
         )
 
         for index, value in enumerate(token_seq):
-            node_properties = {'index': index}
-
-            if index == 0:
-                node_properties['firstToken'] = True
-            elif index == len(token_seq) - 1:
-                node_properties['lastToken'] = True
-
-            pprint.pprint(node_properties)
-
-            misc.run_some_query(
-                CREATE_TOKEN_STATEMENT, {'node_properties': node_properties}
-            )
-
+            self.merge_token(value)
+            self.create_sentence_relationship(token_seq, index, value)
                 
         return result
+
+    def merge_token(self, value):
+        misc.run_some_query(MERGE_TOKEN_STATEMENT, {'token_content': value})
+
+    def create_sentence_relationship(self, token_seq, index, value):
+        rel_properties = {'index': index}
+
+        if index == 0:
+            rel_properties['firstToken'] = True
+        elif index == len(token_seq) - 1:
+            rel_properties['lastToken'] = True
+
+        misc.run_some_query(
+            CREATE_CONTAINS_RELATIONSHIP,
+            {
+                'token_seq': token_seq,
+                'relationship_properties': rel_properties,
+                'token_content': value
+            }
+        )
