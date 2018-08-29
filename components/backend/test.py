3d0da8bb8f71ef7c9775f201bbba389e0f0e3606
David Banks <amoebae@gmail.com>
2018-08-29 17:20:04 +0100

    environment test

--- components/backend/test.py
+++ components/backend/test.py
@@ -1,4 +1,6 @@
 import unittest.mock
+import os
+from logging import debug
 
 class MyNeo4j(object):
     def query(self, string):
@@ -10,3 +12,6 @@ thing.query = unittest.mock.MagicMock(return_value="goodbye")
 def test_query():
     result = thing.query("MATCH (n) RETURN n")
     assert result == 'goodbye'   
+
+def test_can_get_environment():
+    debug("environment is %s", os.environ['OCCUBROW_ENVIRONMENT'])
