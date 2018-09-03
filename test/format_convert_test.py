3d4d48708d980440e22c6c27afe2af917bcd7509
David Banks <amoebae@gmail.com>
2018-09-03 14:25:02 +0100

    removing some obsolete tests

--- test/format_convert_test.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import graph_operations
-
-def test_roots_are_correctly_generated():
-    expected_roots = {'ironmonger', 'hold', 'keep'}
-    actual_roots = graph_operations.get_roots()
-    assert actual_roots == expected_roots
