184fc538282ca2f5bd5aebccd64cd259e2b66fd6
David Banks <amoebae@gmail.com>
2018-11-05 16:16:36 +0000

    testing boltkit demo

--- boltkit_demo.py
+++ boltkit_demo.py
@@ -1,4 +1,8 @@
 import boltkit.controller
+import boltkit.config
+
+realpath = 'ext/neo4j-community-3.4.6'
+
 
 realpath = boltkit.controller._install(
     edition='community',
@@ -6,7 +10,13 @@ realpath = boltkit.controller._install(
     path="ext"
 )
 print("Initialized home at", realpath)
+boltkit.config.update(
+    realpath,
+    {boltkit.config.BOLT_LISTEN_URI_SETTING: 'bolt://127.0.0.1:7687'}
+)
+
 controller = boltkit.controller.create_controller(path=realpath)
 controller.start()
 print("Started!")
 controller.stop()
+
--- makefile
+++ makefile
@@ -2,5 +2,4 @@
 
 
 test:
-	python3 -m pytest test_simple.py
-	python3 -m unittest test_readme.py
+	python3 boltkit_demo.py
