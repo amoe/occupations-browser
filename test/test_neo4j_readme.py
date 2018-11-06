b8a3d2a7b44ec579bfc136227d04b0708292e949
David Banks <amoebae@gmail.com>
2018-11-06 16:02:33 +0000

    installing networkx as dep

--- test/test_neo4j_readme.py
+++ test/test_neo4j_readme.py
@@ -1,5 +1,7 @@
 # This is adapted from test_readme.py in the neo4j-python-driver git repository.
 
+import occubrow.backend
+
 def test_readme_v1(neo4j_driver):
     names = set()
     print = names.add
@@ -18,22 +20,4 @@ def test_readme_v1(neo4j_driver):
     assert len(names) == 1
     assert "Bob" in names
 
-
-# A second version of test_readme that uses assert_graph.
-def test_readme_v2(neo4j_driver):
-    names = set()
-    print = names.add
-
-    def assert_friends(tx, name):
-        for record in tx.run("MATCH (a:Person)-[:KNOWS]->(friend) "
-                             "WHERE a.name = {name} "
-                             "RETURN friend.name", name=name):
-            names.add(record["friend.name"])
-        assert len(names) == 1
-        assert "Bob" in names
-
-    with neo4j_driver.session() as session:
-        session.run("MATCH (a) DETACH DELETE a")
-        session.run("CREATE (a:Person {name:'Alice'})-[:KNOWS]->({name:'Bob'})")
-        session.read_transaction(assert_friends, "Alice")
 
