08e371807c332e3d6529b985b2d48dcbc382dd24
David Banks <amoebae@gmail.com>
2018-11-06 16:39:53 +0000

    more ideas

--- test/test_backend.py
+++ test/test_backend.py
@@ -6,6 +6,9 @@ I wish I had never come here, and I don't want to see no more magic.
 """
 
 # We define our expected node-based representation for the sentence.
+# This will involve a single Sentence node and a bunch of Token nodes.
+# The number of token nodes will be equal to the unique set of tokens derived
+# from tokenizing this sentence.
 
 EXPECTED_DATA = {}
 
