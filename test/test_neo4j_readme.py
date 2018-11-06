96abbb5ac9a75b38afc9dced24031c98cbbc0c71
David Banks <amoebae@gmail.com>
2018-11-06 13:16:31 +0000

    neo4j test fixture revival

---
+++ test/test_neo4j_readme.py
@@ -0,0 +1,19 @@
+# This is adapted from test_readme.py in the neo4j-python-driver git repository.
+
+def test_simple_2(neo4j_driver):
+    names = set()
+    print = names.add
+
+    def print_friends(tx, name):
+        for record in tx.run("MATCH (a:Person)-[:KNOWS]->(friend) "
+                             "WHERE a.name = {name} "
+                             "RETURN friend.name", name=name):
+            print(record["friend.name"])
+
+    with neo4j_driver.session() as session:
+        session.run("MATCH (a) DETACH DELETE a")
+        session.run("CREATE (a:Person {name:'Alice'})-[:KNOWS]->({name:'Bob'})")
+        session.read_transaction(print_friends, "Alice")
+
+    assert len(names) == 1
+    assert "Bob" in names
