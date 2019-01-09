18b23443b9d4d9ed9f946627b7759ab9079db0ba
David Banks <amoebae@gmail.com>
2019-01-09 14:02:07 +0000

    remove checking stuff

--- attempt_samuels_load.py
+++ attempt_samuels_load.py
@@ -9,12 +9,4 @@ ti = taxonomy_inserter.TaxonomyInserter(driver)
 
 loader = SamuelsLoader()
 g = loader.load(sys.argv[1])
-
-
-
-for n, data in g.nodes(data=True):
-    if not data:
-        print("id is '%s'" % n)
-
-
-#ti.load_taxonomy(g)
+ti.load_taxonomy(g)
